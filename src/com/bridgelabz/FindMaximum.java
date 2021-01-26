e116197502f566edc5fe14abf473323bf24f31f0
ajay812998 <ajay812998@gmail.com>
2021-01-26 18:30:58 +0530

    Added : Finding Maximum In Float Values and Test Cases

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -12,9 +12,19 @@ public class FindMaximum {
 		return max;
 	}
 
+	public static Float findMax(Float a, Float b, Float c) {
+		Float max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		return max;
+	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println(findMax(10, 16, 8));
-
+		System.out.println(findMax(10.1f, 16.2f, 8.0f));
 	}
 }
+
