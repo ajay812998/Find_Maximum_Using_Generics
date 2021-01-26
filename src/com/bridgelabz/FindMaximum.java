d015cc817480db046d6d294c6968e0886d018738
ajay812998 <ajay812998@gmail.com>
2021-01-26 17:47:52 +0530

    Added : Welcome to Test Maximum of 3 Variables Program

---
+++ src/com/bridgelabz/FindMaximum.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class FindMaximum {
+	public static void main(String[] args) {
+    System.out.println("Welcome to Test Maximum of three Variables Program");
+  }
+}
