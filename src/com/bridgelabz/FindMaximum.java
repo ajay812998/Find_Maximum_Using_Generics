3b2ac7e391617315159d0d7e5d11cbe778fcf518
ajay812998 <ajay812998@gmail.com>
2021-01-26 18:08:15 +0530

    Added : Finding Maximum In Integer Values and Test Cases

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class FindMaximum {
+	public static Integer findMax(Integer a, Integer b, Integer c) {
+		Integer max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		return max;
+	}
+
 	public static void main(String[] args) {
-    System.out.println("Welcome to Test Maximum of three Variables Program");
-  }
+		Scanner sc = new Scanner(System.in);
+		System.out.println(findMax(10, 16, 8));
+
+	}
 }
